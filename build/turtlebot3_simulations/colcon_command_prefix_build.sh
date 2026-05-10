1fd9bb6e8683423ab13f543cb51cc11bdde90c47
Modhura06 <modhurachatterjee6@gmail.com>
2026-05-10 20:24:46 +0530

    Initial commit

---
+++ build/turtlebot3_simulations/colcon_command_prefix_build.sh
@@ -0,0 +1,4 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/modhura/turtlebot3_ws/install/turtlebot3_fake_node/share/turtlebot3_fake_node/package.sh"
+. "/home/modhura/turtlebot3_ws/install/turtlebot3_gazebo/share/turtlebot3_gazebo/package.sh"
+. "/home/modhura/turtlebot3_ws/install/turtlebot3_manipulation_gazebo/share/turtlebot3_manipulation_gazebo/package.sh"
